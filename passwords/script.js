be83efd9a2ccb642724a7c3a9b1c4c38203744c9
afluffysquirrel <afluffysquirrel@gmail.com>
2017-11-23 17:51:37 +0000

    Update script.js

--- passwords/script.js
+++ passwords/script.js
@@ -1,5 +1,3 @@
-var pass = prompt("Please enter password");
-
 if(CryptoJS.AES.decrypt(data[0].password, pass).toString(CryptoJS.enc.Utf8) != ""){
 	for (i = 0; i < data.length; i++) {
 		document.getElementById("data-area").innerHTML += "<p><center>" + data[i].site + " : " + "<button onclick='alert(" + '"' + CryptoJS.AES.decrypt(data[i].password, pass).toString(CryptoJS.enc.Utf8) + '"' + ")'>View</button>"  + "</center></p>";
