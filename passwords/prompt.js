9a3fbcf5805d2c9f2c8490cc87e158be29dac526
afluffysquirrel <afluffysquirrel@gmail.com>
2017-11-24 07:00:01 +0000

    Update prompt.js

--- passwords/prompt.js
+++ passwords/prompt.js
@@ -1,6 +1,5 @@
 var pass = prompt("Please enter password");
 
-console.log(CryptoJS.AES.decrypt(data[0].password, pass).toString(CryptoJS.enc.Utf8));
 if(CryptoJS.AES.decrypt(data[0].password, pass).toString(CryptoJS.enc.Utf8) === ""){
 	 window.location.href = '../';
 }
