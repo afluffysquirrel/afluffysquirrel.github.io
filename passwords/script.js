58750feb8a31bbacf5148fcd07272e6b53b477d5
afluffysquirrel <afluffysquirrel@gmail.com>
2017-11-28 17:14:18 +0000

    Add files via upload

--- passwords/script.js
+++ passwords/script.js
@@ -1,8 +1,6 @@
 if(CryptoJS.AES.decrypt(data[0].password, pass).toString(CryptoJS.enc.Utf8) != ""){
+	document.getElementById("data-area").innerHTML = "";
 	for (i = 0; i < data.length; i++) {
 		document.getElementById("data-area").innerHTML += "<p><center>" + data[i].site + " : " + "<button onclick='alert(" + '"' + CryptoJS.AES.decrypt(data[i].password, pass).toString(CryptoJS.enc.Utf8) + '"' + ")'>View</button>"  + "</center></p>";
 	}
-}
-else{
-	document.getElementById("data-area").innerHTML = "<center><h1>Error</h1></center>";
 }
